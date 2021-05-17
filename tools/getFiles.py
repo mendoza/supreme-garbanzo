9c9636ca1df39e05a0b87a502f65dfb79e621c62
David Mendoza <d.mendoza99@unitec.edu>
2021-05-17 11:40:59 -0600

    deje el test de 5 files...

--- tools/getFiles.py
+++ tools/getFiles.py
@@ -5,7 +5,7 @@ import wget
 BASE_URL = 'https://ftp.ncbi.nlm.nih.gov/pub/pmc/{}'
 
 with open('./oa_file_list.txt', 'r') as oa:
-    lines = sample(oa.readlines(), 5)
+    lines = sample(oa.readlines(), 1000)
     for file in lines:
         url = file.split('\t')[0]
         fileName = url.split('/')[-1]
